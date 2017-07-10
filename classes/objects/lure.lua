69e56e7e083b613fa2211fd6aaa40ff929b573f2
Xkeeper <xkeeper@gmail.com>
2017-07-09 21:17:27 -0700

    Add the ability to reel in

--- classes/objects/lure.lua
+++ classes/objects/lure.lua
@@ -3,6 +3,9 @@ local Lure	= Class{
 	castLure	= 0,		-- Current phase of lure-casting
 	inWater		= false,	-- Has it hit water yet?
 	hookedThing	= false,	-- Object we've hooked (if anything)
+	lastButton	= false,	-- Was the button pushed last update?
+	holdButton	= false,	-- Is the button pressed this update?
+	catchPoint	= Vector(WATER_WIDTH, WATER_HEIGHT),	-- Where the "catch point" is
 	}
 
 -- The lure is going to do most of the work.
@@ -21,48 +24,31 @@ local Lure	= Class{
 function Lure:update(dt, game)
 
 	-- @TODO Definable buttons or something
-	local holdingButton	= love.keyboard.isDown("z", "x")
+	self.lastButton		= self.holdingButton
+	self.holdingButton	= love.keyboard.isDown("z", "x")
 
-	print(self.castLure, self.inWater, holdingButton, self.velocity)
+
+	--print(self.castLure, self.inWater, self.holdingButton, self.velocity)
 
 	if not self.inWater then
-
-		if self.castLure == 0 and holdingButton then
-			-- launch lure off to upper-left
-			self.velocity	= Vector(-220, -30)
-			self.castLure	= 1
-
-		elseif self.castLure >= 1 and not self.inWater then
-			if holdingButton and self.castLure == 1 then
-				-- "Holding the line open"; lure will fly farther
-				self.velocity.x	= self.velocity.x * .98
-				self.velocity.y	= math.min(40, self.velocity.y + .5)
-			else
-				-- No longer holding it open; lure should stop quickly and sink
-				-- Can't go back to "free flying" after reaching here
-				self.castLure	= 2
-				self.velocity.x	= self.velocity.x * .93
-				self.velocity.y	= math.min(40, self.velocity.y + 1)
-			end
-
-			if self.position.y >= WATER_HEIGHT then
-				-- Hit the water!
-				print("sploosh")
-				self.inWater	= true
-				self.velocity.x	= 0
-				self.velocity.y	= 2
-			end
-		end
+		self:handleCasting()
 
 	elseif self.inWater then
+		self:handleInWater()
 
-		self.velocity.y	= math.min(10, self.velocity.y + 0.1)
-		if self.position.y >= WATER_DEPTH then
-			self.position.y	= WATER_DEPTH
-			self.velocity.y	= 0
+		if self.holdingButton then
+			-- Reel in the lure. Tapping should give
+			-- better results than holding.
+			-- @TODO: Handle different "directions"
+			-- e.g. tap right to gently tug rightward
+			-- or up to nudge more upwards
+
+			local strength	= 10
+			if not self.lastButton then
+				strength	= 75
+			end
+			self:reelIn(strength)
 		end
-
-		-- @TODO Reel in if button pushed. Faster if mashed, slowly if held
 	end
 
 	self:doMovement(dt)
@@ -70,13 +56,63 @@ end
 
 
 
-function Lure:cast()
-	-- @TODO Set movement
+-- Handle the casting of the lure.
+-- If the button is pressed, shoot it out towards water
+-- If not, don't do anything
+function Lure:handleCasting()
+	if self.castLure == 0 and self.holdingButton then
+		-- launch lure off to upper-left
+		self.velocity	= Vector(-220, -30)
+		self.castLure	= 1
+
+	elseif self.castLure >= 1 and not self.inWater then
+		if self.holdingButton and self.castLure == 1 then
+			-- "Holding the line open"; lure will fly farther
+			self.velocity.x	= self.velocity.x * .98
+			self.velocity.y	= math.min(40, self.velocity.y + .5)
+		else
+			-- No longer holding it open; lure should stop quickly and sink
+			-- Can't go back to "free flying" after reaching here
+			self.castLure	= 2
+			self.velocity.x	= self.velocity.x * .93
+			self.velocity.y	= math.min(40, self.velocity.y + 1)
+		end
+
+		if self.position.y >= WATER_HEIGHT then
+			-- Hit the water!
+			print("sploosh")
+			self.inWater	= true
+			self.velocity.x	= 0
+			self.velocity.y	= 2
+		end
+	end
+
+end
+
+
+-- Slow down movement and sink towards the bottom
+-- Stop if we hit the bottom though;
+-- @TODO Stop if we hit the right wall, too
+function Lure:handleInWater()
+	self.velocity.y	= math.min(10, self.velocity.y + 0.1)
+	self.velocity.x	= self.velocity.x * 0.97
+	if self.position.y >= WATER_DEPTH then
+		self.position.y	= WATER_DEPTH
+		self.velocity.x	= 0
+		self.velocity.y	= 0
+	end
 end
 
 
 function Lure:reelIn(strength)
 	-- @TODO Reel line in with given strength
+	-- Get the vector from current position to the corner of the water
+	-- This is NOT the player position b/c the player is slightly farther away
+	-- Then, cap the vector length to the "strength" argument
+	local tempVector	= self.catchPoint - self.position
+	self.velocity		= (tempVector):trimInplace(strength)
+
+	print(self.catchPoint, self.position, tempVector, self.velocity)
 end
 
 
